e37cd1ac5b8f575ea20ceba0657ac87e2e6d21a9
stovermc <stovermc@gmail.com>
2017-07-27 16:40:53 -0600

    Configure CORS
    * added configuration to application.rb as per rack-cots website

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,17 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module HotReads
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
+  module HotReads
+    class Application < Rails::Application
+      config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins '*'
+          resource '*', :headers => :any, :methods => [:get, :post, :options]
+        end
+      end
+      # Settings in config/environments/* take precedence over those specified here.
+      # Application configuration should go into files in config/initializers
+      # -- all .rb files in that directory are automatically loaded.
+    end
+
 end
